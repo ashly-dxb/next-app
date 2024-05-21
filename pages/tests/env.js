3adbae309526453a4ff99dc5d311fd845d7a238b
Ashly Thomas Abraham <ashlythomas@gmail.com>
2024-05-21 09:59:55 +0400

    deployment error fix host env

--- pages/tests/env.js
+++ pages/tests/env.js
@@ -3,6 +3,7 @@
 
 import Head from "next/head";
 // import Container from "../../components/container";
+import styles from "../../container.module.css";
 
 export default function FirstPost(props) {
   return (
@@ -13,21 +14,21 @@ export default function FirstPost(props) {
       </Head>
 
       <h3 className={`${styles.pageHeading}`}>Database Credentials</h3>
-      <p>Host: {props.host}</p>
+      {/* <p>Host: {props.hostname}</p>
       <p>Username: {props.username}</p>
-      <p>Password: {props.password}</p>
+      <p>Password: {props.password}</p> */}
       {/* </Container> */}
     </>
   );
 }
 
-export async function getStaticProps() {
-  // Connect to Database using DB properties
-  return {
-    props: {
-      host: process.env.DB_HOST,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-    },
-  };
-}
+// export async function getStaticProps() {
+//   // Connect to Database using DB properties
+//   return {
+//     props: {
+//       hostname: process.env.DB_HOST,
+//       username: process.env.DB_USER,
+//       password: process.env.DB_PASS,
+//     },
+//   };
+// }
