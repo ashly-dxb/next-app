2ab5e1f8fe6f57dc68b735c60317a33ca304a4c6
Ashly Thomas Abraham <ashlythomas@gmail.com>
2024-05-21 08:51:34 +0400

    user api fix

--- pages/api/user.js
+++ pages/api/user.js
@@ -2,6 +2,6 @@
 
 // To access the API: http://localhost:3000/api/user
 
-export default (req, res) => {
+export default function mytest(req, res) {
   res.status(200).json({ name: "John Doe", age: 28, city: "Dubai" });
-};
+}
