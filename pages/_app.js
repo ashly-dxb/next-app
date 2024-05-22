8a681a80cec7d3ba31d266486614f93aa95f85cf
Ashly Thomas Abraham <ashlythomas@gmail.com>
2024-05-22 10:10:23 +0400

    improvements

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import "../styles/globals.css";
+import Head from "next/head";
 
-function MyTestApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Ashly's Code World</title>
+        <meta name="description" content="Ashly Thomas Abraham's website" />
+        <meta
+          name="keywords"
+          content="Ashly Thomas Abraham, Software Dubai, Web developer, Professional Web Developer"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyTestApp;
